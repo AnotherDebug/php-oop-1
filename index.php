2dfd16e84fc3ee6144bce8e45e852e1c6ef0486a
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-06 14:42:16 +0100

    [ADD]: class Movie

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
 <?php
 
-class Movie {}; 
+class Movie
+{
+    //dichiaro le variabili d'istanza;
+    public $title;
+    public $type;
+    public $nationality;
+    public $description;
+    public $image;
+
+    //dichiaro il costruttore e gli passo le variabili;
+    public function __construct(string $_title, string $_type, string $_nationality, string $_description, $_image)
+    {
+        $this->title = $_title;
+        $this->type = $_type;
+        $this->nationality = $_nationality;
+        $this->description = $_description;
+        $this->image = $_image;
+    }
+};
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <title>Movie</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
+
+<head>
+    <title>Movie</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
 
     <h1>Ciao</h1>
-    
-    </body>
+
+</body>
+
 </html>

adad226aa4df128fd472cd217c2a08191ff54c51
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-06 14:52:24 +0100

    [ADD]: class Media

--- index.php
+++ index.php
@@ -20,6 +20,19 @@ class Movie
     }
 };
 
+class Media
+{
+    public $fileName;
+    public $title;
+    public $type = 'image';
+
+    public function __construct($_fileName, $_title)
+    {
+        $this->fileName = $_fileName;
+        $this->title = $_title;
+    }
+};
+
 ?>
 
 

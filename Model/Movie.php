58f6c420c97d8c6cc30a6aed0b899dfd10a37905
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-06 17:12:46 +0100

    [ADD]:  Model Movie

---
+++ Model/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Movie
+{
+    //dichiaro le variabili d'istanza;
+    public $title;
+    public $type;
+    public $nationality;
+    public $description;
+    public $image;
+
+    //dichiaro il costruttore e gli passo le variabili;
+    public function __construct(string $_title, string $_type, string $_nationality, string $_description, Media $_image = null)
+    {
+        $this->title = $_title;
+        $this->type = $_type;
+        $this->nationality = $_nationality;
+        $this->description = $_description;
+        $this->image = $_image;
+    }
+};

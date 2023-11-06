7b05c66a720f0a980be29721ce38ddbe082c83c1
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-06 14:08:56 +0100

    [INIT]: file index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Movie {}; 
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Movie</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+    <h1>Ciao</h1>
+    
+    </body>
+</html>
